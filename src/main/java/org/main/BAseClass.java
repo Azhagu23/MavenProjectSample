91cc41bfe453b42195891f93ebb40f873f8ed489
Azhagu23 <azhaguar@gmail.com>
2022-09-06 23:11:38 +0530

    Thshan Project

--- src/main/java/org/main/BAseClass.java
+++ src/main/java/org/main/BAseClass.java
@@ -1,5 +1,10 @@
 package org.main;
 
 public class BAseClass {
+	public static void main(String[] args) {
+		System.out.println("Sample 1");
+		System.out.println("Sample 2");
+		System.out.println("Sample 2");
+	}
 
 }
