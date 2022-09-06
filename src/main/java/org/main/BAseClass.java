2bfd97ccfc83b9e0d08728925f997479907322b2
Azhagu23 <azhaguar@gmail.com>
2022-09-06 23:19:11 +0530

    Dhanva code

--- src/main/java/org/main/BAseClass.java
+++ src/main/java/org/main/BAseClass.java
@@ -1,5 +1,11 @@
 package org.main;
 
 public class BAseClass {
+	public static void main(String[] args) {
+		System.out.println("Sample 1");
+		System.out.println("Sample 9");
+		System.out.println("Sample 11");
+		
+	}
 
 }
