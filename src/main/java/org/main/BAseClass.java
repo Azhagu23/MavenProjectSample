fa47014311506e3bf589926ba32ad594b02b183f
Azhagu23 <azhaguar@gmail.com>
2022-09-07 22:34:45 +0530

    Resoled

--- src/main/java/org/main/BAseClass.java
+++ src/main/java/org/main/BAseClass.java
@@ -3,14 +3,10 @@ package org.main;
 public class BAseClass {
 	public static void main(String[] args) {
 		System.out.println("Sample 1");
-<<<<<<< HEAD
+		System.out.println("Sample 2");
 		System.out.println("Sample 9");
 		System.out.println("Sample 11");
-		
-=======
-		System.out.println("Sample 2");
-		System.out.println("Sample 2");
->>>>>>> 2e2b6d94fa3f109d16369f378584d04b36bd0b6d
+
 	}
 
 }
